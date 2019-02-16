d4fbe5ee02cf2908d9f20bf7ab000e64a510cf41
Jan Tristan <jan.tristan@gmail.com>
2019-02-16 13:06:58 -0800

    Make text lines show up one at a time

--- 2020_game.py
+++ 2020_game.py
@@ -1,3 +1,22 @@
+name =input('Name')
+
+print(' Hello, ' + name + '.')
+
+line_1 = " Presidential Election primaries" + " is " + " around the corner."
+line_2 = name , 'presidential candidates are looking for a campaign manager, who perfectly fits their vision and background'
+line_3 = 'Let us begin in the search for your new boss'
+
+
+from time import sleep
+import sys
+
+for x in (line_1,line_2,line_3):
+	print(x, end='',flush=True)  
+	sleep(5)
+
+
+print('Let us begin in the search for your new boss')
+
 import vlc
 import time
 
