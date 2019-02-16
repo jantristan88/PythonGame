f00004ed754dfbda199088d72ccc9a68eed4f38b
Jan Tristan <jan.tristan@gmail.com>
2019-02-16 09:46:29 -0800

    Added the candidates and tested if print from a list works

---
+++ 2020_game.py
@@ -0,0 +1,14 @@
+candidates = [
+	'Donald Trump', 
+	'Bernie Sanders', 
+	'Joe Biden', 
+	'Kamala Harris', 
+	'Elizabeth Warren',
+	'Cory Booker',
+	'Beto ORourke',
+	'Julian Castro',
+	'Tulsi Gabbard',
+	'Amy Klobuchar',
+	'Kirsten Gillibrand',]
+
+print(candidates[0])
